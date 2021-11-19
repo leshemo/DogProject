dfe9e03fafacc45dc10e8ce1c2037074069e9e9c
leshemo <81444861+leshemo@users.noreply.github.com>
2021-11-19 12:21:22 -0500

    votingSystem

--- src/VotingSystems/IVotingSystem.java
+++ src/VotingSystems/IVotingSystem.java
@@ -1,11 +1,17 @@
 package VotingSystems;
 
+import java.util.Map;
+
+import VoterPopulation.BasicAgent;
+
 /**
  * Interface representing voting systems, in which each one creates or takes in a VoterList and runs
  * the voting system on that list, returning results to a resultsList.
  */
 public interface IVotingSystem {
 
-  
+
+  //method to return results, probably as a list of agent utilities of the results
+  public Map<BasicAgent, Integer> getVotingResult();
 
 }
