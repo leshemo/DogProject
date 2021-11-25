fba4f26dcb1550019958beac63f61b2d84118f16
leshemo <81444861+leshemo@users.noreply.github.com>
2021-11-24 20:04:05 -0500

    Update PluralityVoting.java

--- src/VotingSystems/PluralityVoting.java
+++ src/VotingSystems/PluralityVoting.java
@@ -9,6 +9,7 @@ import VoterPopulation.VoterList;
 
 public class PluralityVoting implements IVotingSystem {
   private final VoterList votes;
+  private Map<String, Integer> pluralityResult = new HashMap<>(); //might not be good choice
   private final Map<String, Double> resultList = new HashMap<>();
 
   public PluralityVoting(VoterList votes) {
@@ -22,10 +23,24 @@ public class PluralityVoting implements IVotingSystem {
   }
 
   //needs to grab the first candidate of each agent, and tally them up, returning that tally to the
-  //resultList
+  //pluralityResult list
   private final void calculateResult() {
+    //put all the candidates into the pluralityResult list, then for each basic agent, get their
+    //top utility and add it to the appropriate result in plurality list
+
+    for (String cand : this.votes.getCandidateList()) {
+      this.pluralityResult.put(cand, 0);
+    }
 
     for (BasicAgent b : this.votes.getAgentList()) {
+      //grab first entry in hashmap
+      String chosen = b.getRanking().get("FIRST");
+      for (int i = 0; i < this.pluralityResult.size(); i++) {
+        if (chosen.equals(this.pluralityResult.get(i))) {  //same thing as above fix
+         this.pluralityResult.get()
+        }
+        this.pluralityResult.get(chosen) =+ 1;
+      }
 
 
     }
